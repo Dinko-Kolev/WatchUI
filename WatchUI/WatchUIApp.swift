0f033302b6876d669639f6bf278ab7e56e3f7a62
Dinko <dinkonikolaevkolev@gmail.com>
2024-04-06 01:28:09 +0200

    Initial Commit

---
+++ WatchUI/WatchUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchUIApp.swift
+//  WatchUI
+//
+//  Created by Dinko Nikolaev Kolev on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WatchUIUITests/WatchUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WatchUIUITestsLaunchTests.swift
+//  WatchUIUITests
+//
+//  Created by Dinko Nikolaev Kolev on 6/4/24.
+//
+
+import XCTest
+
+final class WatchUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
